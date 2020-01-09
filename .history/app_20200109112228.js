cd7e03708c9507f3ea309b3b38779e2f99fafa0c
davet <daveenq@gmail.com>
2020-01-09 14:50:00 +0000

    Basic swagger project

---
+++ .history/app_20200109112228.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+
+const yaml = require('yamljs')
+const swaggerUi = require('swagger-ui-express')
+
+const swaggerDoc = yaml.load('./api/swagger.yml')
+
+
+
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+  // install middleware
+  swaggerExpress.register(app);
+
+  var port = process.env.PORT || 4000;
+  app.listen(port);
+
+  if (swaggerExpress.runner.swagger.paths['/hello']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  }
+});
